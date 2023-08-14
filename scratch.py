f7dfaf8be631f565668b795affa2bd95516b74c3
composercordero <composercordero@gmail.com>
2023-08-14 12:15:26 -0500

    Initial commit with Flask setup

---
+++ scratch.py
@@ -0,0 +1,21 @@
+def my_decorator(func):
+    def wrapper():
+        print('this is happening before the function is called')
+        func()
+        print('this is happening after the function is called')
+    return wrapper
+
+def say_hello():
+    print('Hello, How are you today?')
+
+say_hello = my_decorator(say_hello)
+
+say_hello()
+
+print('-'*100)
+
+@my_decorator
+def say_goodbye():
+    print('goodbye')
+
+say_goodbye()
