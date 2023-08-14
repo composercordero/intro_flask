75fabebb3719387b3b6ba2777a9df0dc9d4bab80
composercordero <composercordero@gmail.com>
2023-08-14 15:28:06 -0500

    Added base.html for template inheritance + sig                                                  g nup route and template

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,6 @@ def index():
     numbers = [1,2,3,4,5]
     return render_template('index.html', first_name = 'Carlos', numbers = numbers)
 
-@app.route('/new')
-def new():
-    return 'This is a new modified route!'
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
